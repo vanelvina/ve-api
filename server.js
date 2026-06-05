8efff99a68c3c6331248036b10909d1c0c70504b
saqebkhan <saqeb.khan@thesouledstore.com>
2026-06-05 15:34:43 +0530

    CORS

--- server.js
+++ server.js
@@ -23,10 +23,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
